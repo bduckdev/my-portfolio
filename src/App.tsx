df44fbda94b34190ad66c31ad598c2fb014d2bff
Brennan Duck <brennantduck@gmail.com>
2023-12-24 22:39:29 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Brennan Duck</h1>
+      <div className="card">
+      TEST
+      </div>
+    </>
+  )
+}
+
+export default App
